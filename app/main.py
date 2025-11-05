fdab359c9ded7d9c29058c34c14638f67f10f098
Oleksandr <15bizon15@gmail.com>
2025-11-05 19:07:08 +0100

    Updated import, main.py and cafe.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+from app.errors import (
+    VaccineError,
+    NotWearingMaskError
+)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
@@ -13,6 +16,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         except NotWearingMaskError:
             masks_to_buy += 1
 
-    if masks_to_buy > 0:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
+
     return f"Friends can go to {cafe.name}"
