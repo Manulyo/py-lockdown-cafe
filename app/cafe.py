3e1e1a94a0b920b1ced4945dea1d3a7f0acd9269
Oleksandr <15bizon15@gmail.com>
2025-11-05 19:14:57 +0100

    Fix f-string in cafe

--- app/cafe.py
+++ app/cafe.py
@@ -14,18 +14,18 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"{visitor.get("name", "Visitor")} is not vaccinated!"
+                f'{visitor.get("name", "Visitor")} is not vaccinated!'
             )
 
         expiration_date = visitor["vaccine"]["expiration_date"]
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError(
-                f"{visitor.get("name", "Visitor")}'s vaccine is outdated!"
+                f'{visitor.get("name", "Visitor")} vaccine is outdated!'
             )
 
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError(
-                f"{visitor.get("name", "Visitor")} is not wearing a mask!"
+                f'{visitor.get("name", "Visitor")} is not wearing a mask!'
             )
 
         return f"Welcome to {self.name}"
